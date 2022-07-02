a4a1183708c768f91c2b1677fc227d56eebaf4b4
Joao Oliveira <vitinho.oliv@gmail.com>
2022-07-01 22:56:39 -0300

    feat: creating project base

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nodets-kennel
+Project made on NODE + TYPESCRIPT course
+
+### Global prerequisites:
+`npm i -g nodemon typescript ts-node`
+
+### Install
+`npm install`
+
+### Run Project
+`npm run start-dev`
---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path from 'path';
+import mainRoutes from './routes/index';
+
+dotenv.config();
+
+const server = express();
+
+server.set('view engine', 'mustache'); // Set mustache as view engine
+server.set ('views', path.join(__dirname, 'views')); // Set view folder path
+server.engine('mustache', mustache()); // Set engine
+
+server.use(express.static(path.join(__dirname, '../public'))); // Set public folder path
+
+// Routes
+server.use(mainRoutes);
+
+server.use((req, res) => {
+    res.send('Page not found');
+})
+
+server.listen(process.env.PORT);
